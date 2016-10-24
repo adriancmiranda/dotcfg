3da5ad1c21ce6f32294754a460b8269d15422ffe
Adrian Miranda <adriancmiranda@gmail.com>
2016-10-24 02:25:32 -0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,10 @@
-declare module "dotcfg";
+interface DotCFG {
+	cfg(key?: string|boolean|Object, value?: any, strategy?: Function): DotCFG;
+	(namespace?: string|Object, target?: Object, strategy?: Function): any
+}
+
+declare var dotcfg: DotCFG;
+
+declare module 'dotcfg' {
+	export = dotcfg;
+}
