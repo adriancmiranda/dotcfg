af52f5488e45fb72f1d689ef8bd5140cb67b339e
Adrian Miranda <adriancmiranda@gmail.com>
2016-10-24 02:29:50 -0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 interface DotCFG {
 	cfg(key?: string|boolean|Object, value?: any, strategy?: Function): DotCFG;
-	(namespace?: string|Object, target?: Object, strategy?: Function): any
+	(namespace?: string|Object, target?: Object, strategy?: Function): any;
 }
 
 declare var dotcfg: DotCFG;
