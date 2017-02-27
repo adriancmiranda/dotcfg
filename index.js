e4be38f4351cc98946ca11da4678edbd6f807df3
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-02-27 01:57:36 -0300

    Update v1.5.3

--- index.js
+++ index.js
@@ -86,15 +86,23 @@
 	function assign(target){
 		var args = Array.prototype.slice.call(arguments);
 		var output = Object(target || {});
-		for (var ix = 1; ix < args.length; ix++) {
-			var from = args[ix];
-			var keys = Object.keys(Object(from));
-			for (var iy = 0; iy < keys.length; iy++) {
-				var key = keys[iy];
-				if (Array.isArray(from[key])) {
-					from[key] = from[key].slice();
+		for (var ix = 1, from, keys, cpath; ix < args.length; ix++) {
+			from = args[ix];
+			keys = Object.keys(Object(from));
+			cpath = '';
+			for (var iy = 0, key, o, f; iy < keys.length; iy++) {
+				key = keys[iy];
+				if (Array.isArray(output[key]) || Array.isArray(from[key])) {
+					o = (Array.isArray(output[key]) ? output[key].slice() : []);
+					f = (Array.isArray(from[key]) ? from[key].slice() : []);
+					output[key] = assingStrategy(output[key], o.concat(f), cpath, keys);
+				} else if (isFunction(output[key]) || isFunction(from[key])) {
+					output[key] = assingStrategy(output[key], from[key], cpath, keys);
+				} else if (isObject(output[key]) || isObject(from[key])) {
+					output[key] = assign(output[key], from[key]);
+				} else {
+					output[key] = assingStrategy(output[key], from[key], cpath, keys);
 				}
-				write(output, key, from[key], assingStrategy);
 			}
 		}
 		return output;
