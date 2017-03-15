90fcb94cbdba0c57c1e540c8ef6ea7e5b900b46c
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-15 16:24:20 -0300

    Adding webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,35 @@
-const path = require('path');
-const webpack = require('webpack');
+const { resolve } = require('path');
+const { optimize } = require('webpack');
+const pkg = require('./package.json');
 
-module.exports = {};
+module.exports = (argv = {}) => ({
+	context: __dirname,
+	entry: 'index.js',
+	devtool: 'source-map',
+	output: {
+		path: 'dist',
+		filename: `${pkg.name}${argv.dev ? '.min' : ''}.js`,
+		library: pkg.name,
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	resolve: {
+		modules: [resolve('source')],
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				loader: 'xo-loader',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				options: {
+					emitError: true,
+				},
+			},
+		],
+	},
+	plugins: argv.dev ? [] : [
+		new optimize.UglifyJsPlugin({ minimize: true }),
+	],
+});
