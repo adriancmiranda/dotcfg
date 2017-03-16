07c134867485ed2132bceaa32f21a708499ee30b
Adrian Miranda <adriancmiranda@gmail.com>
2017-03-16 14:22:59 -0300

    Update modules

--- source/core/parse.js
+++ source/core/parse.js
@@ -1,6 +1,14 @@
-// var
+var spaces = /\s/g;
+var objectAssessor = /\[(["']?)([^\1]+?)\1?\]/g;
+var startWithDot = /^\./;
+
+function replacer(match, p1, p2) {
+  return (isNaN(p2) ? ' ' : '.') + p2;
+}
 
 module.exports = function parse(path) {
-	const keys = path.split('.');
-	return keys;
+  var keys = path.replace(spaces, '').replace(objectAssessor, replacer);
+  keys = keys.replace(startWithDot, '').split(spaces);
+  keys = keys.length > 1 ? keys.filter(String) : keys[0].split('.');
+  return keys;
 };
