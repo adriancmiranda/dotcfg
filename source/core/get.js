669f624afb9fde931565041a103817d40d3a518d
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-16 01:39:54 -0300

    Adding get method

--- source/core/get.js
+++ source/core/get.js
@@ -1,1 +1,7 @@
-module.exports = {};
+var parse = require('./parse');
+
+module.exports = function read(scope, path) {
+  var id = 0, keys = parse(path), total = keys.length;
+  while ((scope = scope[keys[id++]]) && id < total) {}
+  return id < total ? undefined : scope;
+};
--- test/resolve.spec.js
+++ test/resolve.spec.js
@@ -3,13 +3,12 @@ import dotcfg from '../';
 import res from '../source/core/resolve';
 
 ava('resolve', t => {
+  // call a.b.c function with arguments 'argA', 'argB' and 'argC'
   const argA = 'argA';
   const argB = 'argB';
   const argC = 'argC';
   const fn = (a, b, c) => ({ a: a, b: b, c: c });
   let resolve = res({ a: { b: { c: fn } } });
-
-  // call a.b.c function with arguments 'argA', 'argB' and 'argC'
   t.deepEqual(resolve('a.b.c', argA, argB, argC), {
     a: argA,
     b: argB,
@@ -19,5 +18,5 @@ ava('resolve', t => {
   // get a.b.c property value
   const propertyValue = 'This is a test';
   resolve = res({ a: { b: { c: propertyValue } } });
-  t.is(resolve('a.b.c'), propertyValue)
+  t.is(resolve('a.b.c'), propertyValue);
 });
