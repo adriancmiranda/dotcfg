9491de63c89fde0c50594a318fe8ee593a74c69e
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-17 06:01:44 -0300

    Expected tests to assign

--- test/assign.spec.js
+++ test/assign.spec.js
@@ -1,5 +1,59 @@
 import ava from 'ava';
 import dotcfg from '../';
 
-ava('assign', t => {
+ava('assign:can extend on 1 level', t => {
+});
+
+ava('assign:can extend on 2 levels', t => {
+});
+
+ava('assign:can extend with Buffer values', t => {
+});
+
+ava('assign:Buffer is cloned', t => {
+});
+
+ava('assign:Date objects', t => {
+});
+
+ava('assign:Date object is cloned', t => {
+});
+
+ava('assign:RegExp objects', t => {
+});
+
+ava('assign:RegExp object is cloned', t => {
+});
+
+ava('assign:does not change sources', t => {
+});
+
+ava('assign:clone arrays instead of extend', t => {
+});
+
+ava('assign:deep merge arrays', t => {
+});
+
+ava('assign:deep merge arbitrary types', t => {
+});
+
+ava('assign:deep merge array and value', t => {
+});
+
+ava('assign:gets key for the merging', t => {
+});
+
+ava('assign:deep merging works', t => {
+});
+
+ava('assign:multi merge works', t => {
+});
+
+ava('assign:deep merge objects', t => {
+});
+
+ava('assign:recursive clone objects and special objects in cloned arrays', t => {
+});
+
+ava('assign:checking keys for hasOwnPrototype', t => {
 });
