b8f2e7f3dbdeb6ec33ac7c61dbd89379f8612c2c
Adrian Miranda <adriancmiranda@gmail.com>
2017-03-17 11:04:46 -0300

    Assign with strategy

--- index.js
+++ index.js
@@ -31,17 +31,23 @@ function write(target, path, value, strategy){
 function assign(target){
   var args = Array.prototype.slice.call(arguments);
   var output = Object(target || {});
-  for (var ix = 1, from, keys; ix < args.length; ix++) {
-    from = args[ix];
-    keys = Object.keys(Object(from));
-    for (var iy = 0, key; iy < keys.length; iy++) {
-      key = keys[iy];
-      if (Array.isArray(from[key])) {
-        output[key] = from[key].slice();
+  for (var ix = 1; ix < args.length; ix++) {
+    var from = args[ix];
+    var keys = Object.keys(Object(from));
+    var cpath;
+    for (var iy = 0; iy < keys.length; iy++) {
+      var key = cpath = keys[iy];
+      if (Array.isArray(output[key]) || Array.isArray(from[key])) {
+        var o = Array.isArray(output[key]) ? output[key].slice() : [];
+        var f = Array.isArray(from[key]) ? from[key].slice() : [];
+        output[key] = assignStrategy(output[key], o.concat(f), cpath, keys);
+      } else if (is.fn(output[key]) || is.fn(from[key])) {
+        output[key] = assignStrategy(output[key], from[key], cpath, keys);
       } else if (is.object(output[key]) || is.object(from[key])) {
         output[key] = assign(output[key], from[key]);
+      } else {
+        output[key] = assignStrategy(output[key], from[key], cpath, keys);
       }
-      write(output, key, output[key], assignStrategy);
     }
   }
   return output;
--- source/core/assign.js
+++ source/core/assign.js
@@ -1,24 +1,26 @@
-module.exports = function(target){
-  var args = Array.prototype.slice.call(arguments);
-  var output = Object(target || {});
-  for (var ix = 1; ix < args.length; ix++) {
-    var from = args[ix];
-    var keys = Object.keys(Object(from));
-    var cpath = '';
-    for (var iy = 0; iy < keys.length; iy++) {
-      var key = keys[iy];
-      if (Array.isArray(output[key]) || Array.isArray(from[key])) {
-        var o = (Array.isArray(output[key]) ? output[key].slice() : []);
-        var f = (Array.isArray(from[key]) ? from[key].slice() : []);
-        output[key] = assignStrategy(output[key], o.concat(f), cpath, keys);
-      } else if (is.fn(output[key]) || is.fn(from[key])) {
-        output[key] = assignStrategy(output[key], from[key], cpath, keys);
-      } else if (is.object(output[key]) || is.object(from[key])) {
-        output[key] = assign(output[key], from[key]);
-      } else {
-        output[key] = assignStrategy(output[key], from[key], cpath, keys);
-      }
-    }
-  }
-  return output;
+module.exports = function(strategy){
+	return function(target){
+	  var args = Array.prototype.slice.call(arguments);
+	  var output = Object(target || {});
+	  for (var ix = 1; ix < args.length; ix++) {
+	    var from = args[ix];
+	    var keys = Object.keys(Object(from));
+	    var cpath;
+	    for (var iy = 0; iy < keys.length; iy++) {
+	      var key = cpath = keys[iy];
+	      if (Array.isArray(output[key]) || Array.isArray(from[key])) {
+	        var o = (Array.isArray(output[key]) ? output[key].slice() : []);
+	        var f = (Array.isArray(from[key]) ? from[key].slice() : []);
+	        output[key] = strategy(output[key], o.concat(f), cpath, keys);
+	      } else if (is.fn(output[key]) || is.fn(from[key])) {
+	        output[key] = strategy(output[key], from[key], cpath, keys);
+	      } else if (is.object(output[key]) || is.object(from[key])) {
+	        output[key] = assign(output[key], from[key]);
+	      } else {
+	        output[key] = strategy(output[key], from[key], cpath, keys);
+	      }
+	    }
+	  }
+	  return output;
+	};
 };
