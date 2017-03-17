67a321b81bfaa1cffc4d0e5fdb1d12d5b3d968a9
Adrian Miranda <adriancmiranda@gmail.com>
2017-03-17 11:05:27 -0300

    Assign with strategy

--- index.js
+++ index.js
@@ -38,9 +38,8 @@ function assign(target){
     for (var iy = 0; iy < keys.length; iy++) {
       var key = cpath = keys[iy];
       if (Array.isArray(output[key]) || Array.isArray(from[key])) {
-        var o = Array.isArray(output[key]) ? output[key].slice() : [];
         var f = Array.isArray(from[key]) ? from[key].slice() : [];
-        output[key] = assignStrategy(output[key], o.concat(f), cpath, keys);
+        output[key] = assignStrategy(output[key], f, cpath, keys);
       } else if (is.fn(output[key]) || is.fn(from[key])) {
         output[key] = assignStrategy(output[key], from[key], cpath, keys);
       } else if (is.object(output[key]) || is.object(from[key])) {
