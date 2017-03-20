796a63d466f5644711a95c62cbb39cc9c0c9f212
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-20 01:27:09 -0300

    style: nomenclature

--- source/write.js
+++ source/write.js
@@ -2,24 +2,29 @@ var parse = require('./parse');
 var isDefined = require('./is/defined');
 var isObjectLike = require('./is/object-like');
 
-module.exports = function(target, path, value, strategy) {
+module.exports = function(target, notation, value, strategy) {
 	var id = 0;
 	var dot = target;
-	var notation = path;
-	var keys = parse(path);
+	var notation = notation;
+	var keys = parse(notation);
 	var total = keys.length - 1;
 	while (id < total) {
-		path = keys[id++];
-		if (isObjectLike(target[path])) {
-			target = target[path];
+		notation = keys[id++];
+		if (isObjectLike(target[notation])) {
+			target = target[notation];
 		} else {
-			target[path] = Object.create(null);
-			target = target[path];
+			target[notation] = Object.create(null);
+			target = target[notation];
 		}
 	}
-	path = keys[id];
+	notation = keys[id];
 	if (isDefined(value)) {
-		target[path] = strategy(value, target[path], notation, keys);
-	} else delete(target[path]);
+		target[notation] = strategy(
+			value,
+			target[notation],
+			notation,
+			keys
+		);
+	} else delete(target[notation]);
 	return dot;
 };
