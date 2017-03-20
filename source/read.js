b604ffc5caafaae392ffd09fbf9aa30f78a883b3
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-20 01:25:54 -0300

    style: nomenclature

--- source/read.js
+++ source/read.js
@@ -1,8 +1,8 @@
 var parse = require('./parse');
 
-module.exports = function(scope, path) {
+module.exports = function(scope, notation) {
   var id = 0;
-  var keys = parse(path);
+  var keys = parse(notation);
   var total = keys.length;
   while ((scope = scope[keys[id++]]) && id < total) {/*!*/}
   return id < total ? undefined : scope;
