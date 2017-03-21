5ef0b9620f7bef05b9f03922e89a0097dc1f65f6
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-03-20 22:13:08 -0300

    fix: write array

--- source/parse/index.js
+++ source/parse/index.js
@@ -1,4 +1,5 @@
 var parseNotation = require('./notation');
+var isNumeric = require('../is/numeric');
 
 var hasBrackets = /\[|\]/;
 var parts = /(\[{1}\s{0,1})(.{0,}?\]{0,})(\s{0,1}\]{1})/g;
@@ -8,6 +9,11 @@ module.exports = function (path) {
 	for (var x = 0; x < notation.length; x++) {
 		if (hasBrackets.test(notation[x])) {
 			notation[x] = notation[x].replace(parts, ',$2').split(',');
+			for (var y = 1; y <= notation[x].length; y++) {
+				if (isNumeric(notation[x][y])) {
+					notation[x][y] = parseInt(notation[x][y], 10);
+				}
+			}
 		}
 	}
 	return Array.prototype.concat.apply([], notation);
