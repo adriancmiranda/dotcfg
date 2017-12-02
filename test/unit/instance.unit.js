facc67d98f6e5981c422a6684137b883fae90c51
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-12-02 17:41:56 -0200

    test: ava -> jasmine

--- test/unit/instance.unit.js
+++ test/unit/instance.unit.js
@@ -7,34 +7,34 @@ describe('dotcfg', () => {
   });
 
   it('new instance', () => {
-    // // const TEST_CHAIN = dotcfg({}).set('name', 'test').cfg('env', 'ava');
-    // // t.is(TEST_CHAIN.get('env'), 'ava');
-    // // t.deepEqual(TEST_CHAIN.cfg(), { name: 'test', env: 'ava' });
-    // // t.falsy(TEST_CHAIN instanceof dotcfg);
+    // const TEST_CHAIN = dotcfg({}).set('name', 'test').cfg('env', 'ava');
+    // t.is(TEST_CHAIN.get('env'), 'ava');
+    // t.deepEqual(TEST_CHAIN.cfg(), { name: 'test', env: 'ava' });
+    // t.falsy(TEST_CHAIN instanceof dotcfg);
 
-    // const TEST_TYPE = dotcfg({});
-    // TEST_TYPE.set('name', 'test').cfg('env', 'ava')
-    // t.deepEqual(TEST_TYPE.cfg(), { name: 'test', env: 'ava' });
-    // // t.truthy(TEST_TYPE instanceof dotcfg); // @TODO < fix this
+    const TEST_TYPE = dotcfg({});
+    TEST_TYPE.set('name', 'test').cfg('env', 'ava');
+    expect(TEST_TYPE.cfg()).toEqual({ name: 'test', env: 'ava' });
+    // t.truthy(TEST_TYPE instanceof dotcfg); // @TODO < fix this
 
-    // const TEST_OBJ = dotcfg({});
-    // t.truthy(TEST_OBJ.cfg, 'instance.cfg function exists');
-    // t.truthy(TEST_OBJ.res, 'instance.res function exists');
-    // t.truthy(TEST_OBJ.exe, 'instance.exe function exists');
-    // t.truthy(TEST_OBJ.get, 'instance.get function exists');
-    // t.truthy(TEST_OBJ.set, 'instance.set function exists');
+    const TEST_OBJ = dotcfg({});
+    expect(TEST_OBJ.cfg).toEqual(jasmine.any(Function), 'instance.cfg function exists');
+    expect(TEST_OBJ.res).toEqual(jasmine.any(Function), 'instance.res function exists');
+    expect(TEST_OBJ.exe).toEqual(jasmine.any(Function), 'instance.exe function exists');
+    expect(TEST_OBJ.get).toEqual(jasmine.any(Function), 'instance.get function exists');
+    expect(TEST_OBJ.set).toEqual(jasmine.any(Function), 'instance.set function exists');
 
-    // const TEST_NS = dotcfg('TEST_NS');
-    // t.truthy(TEST_NS.cfg, 'instance.cfg function exists');
-    // t.truthy(TEST_NS.res, 'instance.res function exists');
-    // t.truthy(TEST_NS.exe, 'instance.exe function exists');
-    // t.truthy(TEST_NS.get, 'instance.get function exists');
-    // t.truthy(TEST_NS.set, 'instance.set function exists');
+    const TEST_NS = dotcfg('TEST_NS');
+    expect(TEST_NS.cfg).toEqual(jasmine.any(Function), 'instance.cfg function exists');
+    expect(TEST_NS.res).toEqual(jasmine.any(Function), 'instance.res function exists');
+    expect(TEST_NS.exe).toEqual(jasmine.any(Function), 'instance.exe function exists');
+    expect(TEST_NS.get).toEqual(jasmine.any(Function), 'instance.get function exists');
+    expect(TEST_NS.set).toEqual(jasmine.any(Function), 'instance.set function exists');
 
-    // // const SCOPE = { name: 'NS scope' };
-    // // const TEST_NS_SCOPE = dotcfg('TEST_NS', SCOPE);
-    // // TEST_NS_SCOPE.cfg('world', 'hello');
-    // // t.truthy(SCOPE.TEST_NS.world, 'NS scope');
+    // const SCOPE = { name: 'NS scope' };
+    // const TEST_NS_SCOPE = dotcfg('TEST_NS', SCOPE);
+    // TEST_NS_SCOPE.cfg('world', 'hello');
+    // t.truthy(SCOPE.TEST_NS.world, 'NS scope');
   });
 
  dotcfg('instance.override', () => {
@@ -45,62 +45,62 @@ describe('dotcfg', () => {
       get: 'my get property',
       set: 'my set property',
     };
-    // const TEST_NEW = new dotcfg(obj);
+    const TEST_NEW = new dotcfg(obj);
 
-    // t.truthy(typeof TEST_NEW.get('res') === 'function');
-    // t.is(TEST_NEW.get('@res'), 'my res property');
+    expect(TEST_NEW.get('res')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@res')).toEqual('my res property');
 
-    // t.truthy(typeof TEST_NEW.get('exe') === 'function');
-    // t.is(TEST_NEW.get('@exe'), 'my exe property');
+    expect(TEST_NEW.get('exe')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@exe')).toEqual('my exe property');
 
-    // t.truthy(typeof TEST_NEW.get('cfg') === 'function');
-    // t.is(TEST_NEW.get('@cfg'), 'my cfg property');
+    expect(TEST_NEW.get('cfg')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@cfg')).toEqual('my cfg property');
 
-    // t.truthy(typeof TEST_NEW.get('get') === 'function');
-    // t.is(TEST_NEW.get('@get'), 'my get property');
+    expect(TEST_NEW.get('get')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@get')).toEqual('my get property');
 
-    // t.truthy(typeof TEST_NEW.get('set') === 'function');
-    // t.is(TEST_NEW.get('@set'), 'my set property');
+    expect(TEST_NEW.get('set')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@set')).toEqual('my set property');
 
-    // t.deepEqual(TEST_NEW.cfg(true), {
-    //   res: 'my res property',
-    //   exe: 'my exe property',
-    //   cfg: 'my cfg property',
-    //   get: 'my get property',
-    //   set: 'my set property',
-    // }, 'getting everything cleaner');
+    expect(TEST_NEW.cfg(true)).toEqual({
+      res: 'my res property',
+      exe: 'my exe property',
+      cfg: 'my cfg property',
+      get: 'my get property',
+      set: 'my set property',
+    }, 'getting everything cleaner');
 
-    // TEST_NEW.cfg('@res', 'res property change');
-    // // TEST_NEW.cfg('res', 10); // @TODO < fix this
+    TEST_NEW.cfg('@res', 'res property change');
+    // TEST_NEW.cfg('res', 10); // @TODO < fix this
 
-    // t.truthy(typeof TEST_NEW.get('res') === 'function');
-    // t.is(TEST_NEW.get('@res'), 'res property change');
+    expect(TEST_NEW.get('res')).toEqual(jasmine.any(Function));
+    expect(TEST_NEW.get('@res')).toEqual('res property change');
 
-    // t.deepEqual(TEST_NEW.cfg(true), {
-    //   res: 'res property change',
-    //   exe: 'my exe property',
-    //   cfg: 'my cfg property',
-    //   get: 'my get property',
-    //   set: 'my set property',
-    // }, 'getting everything cleaner');
+    expect(TEST_NEW.cfg(true)).toEqual({
+      res: 'res property change',
+      exe: 'my exe property',
+      cfg: 'my cfg property',
+      get: 'my get property',
+      set: 'my set property',
+    }, 'getting everything cleaner');
 
-    // t.is(TEST_NEW.cfg(), obj);
+    expect(TEST_NEW.cfg()).toEqual(obj);
   });
 
-  it('instance.set', () => {
+  // it('instance.set', () => {
     // const i = dotcfg({});
     // t.is(i.cfg('test', 'test property value').cfg('test'), 'test property value');
     // t.is(i.cfg('another.property', 'another property value').cfg('another.property'), 'another property value');
-  });
+  // });
 
-  it('instance.get', () => {
+  // it('instance.get', () => {
     // const i = dotcfg({ name: 'instance.get' });
     // t.is(i.cfg('name'), 'instance.get');
     // t.is(i.cfg(true).name, 'instance.get', 'copy works');
     // t.is(i.cfg().name, 'instance.get');
-  });
+  // });
 
-  it('instance.assign', () => {
+  // it('instance.assign', () => {
     // const common = dotcfg({ name: 'common' });
     // common.cfg('entry', 'entry common')
     // common.cfg('commonProp', 'common property');
@@ -120,5 +120,5 @@ describe('dotcfg', () => {
     // t.is(merges[1].commonProp, 'common property');
     // t.is(merges[1].entry, 'entry common');
     // t.is(merges[1].name, 'common');
-  });
+  // });
 });
