2a88a01e99c89d44b391be4fb955f6e4c52dcf8a
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-12-02 15:27:18 -0200

    fix: tasks

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as core from './source/core/index.js';
+import * as strategies from './source/strategies/index.js';
+
+export { core, strategies };
+export { default as parse } from './source/parse/index.js';
+export { default as DotCfg } from './source/index.js';
