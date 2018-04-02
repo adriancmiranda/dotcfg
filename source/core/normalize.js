ccdf7d3bad55ba1643349ac3658bb88344863477
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-04-01 21:41:41 -0300

    chore: recursive option to normalize

--- source/core/normalize.js
+++ source/core/normalize.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-restricted-syntax */
 import ownProperty from 'describe-type/source/has/ownProperty.js';
+import as from 'describe-type/source/as/as.any.js';
 import object from 'describe-type/source/is/object.js';
 import deletePropertyAt from '../@/deletePropertyAt.js';
+import dotStrategyDefault from './strategies/dotDefault.js';
 import write from './write.js';
 
 export default function normalize(hash, strategy, recursive) {
+	hash = as(Object, hash, {});
+	strategy = as(Function, strategy, dotStrategyDefault);
+	recursive = as(Boolean, recursive, true);
 	for (const key in hash) {
 		if (ownProperty(hash, key)) {
 			if (recursive && object(hash[key])) {
